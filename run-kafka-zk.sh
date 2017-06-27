e52f4168beda0cf8efe76bd3e6fc389d4d65de6a
Adam Lamers <adamlamers@gmail.com>
2017-06-27 20:06:29 +0000

    Add number of partitions to run-kafka-zk.sh

--- run-kafka-zk.sh
+++ run-kafka-zk.sh
@@ -7,12 +7,14 @@ export ADVERTISED_HOST=$(hostname)
 export BROKER_ID=$1
 export ZK_SERVERS="kafka-test-1,kafka-test-2,kafka-test-3"
 export ZK_ID=$1
+export NUM_PARTITIONS=105
 
 docker run -d \
 	-e ADVERTISED_HOST \
 	-e BROKER_ID \
 	-e ZK_SERVERS \
 	-e ZK_ID \
+	-e NUM_PARTITIONS \
 	-p 2181:2181 \
 	-p 2888:2888 \
 	-p 3888:3888 \
